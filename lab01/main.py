bf7681efa8c27af801e6f4f694bfe3d98af1833f
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-10-05 17:44:28 +0200

    add recommender to project

--- lab01/main.py
+++ lab01/main.py
@@ -2,6 +2,8 @@ import os
 import sqlalchemy
 import logging
 import datetime
+from recommender import *
+import pandas as pd
 from flask import jsonify
 
 logger = logging.getLogger()
@@ -57,13 +59,16 @@ db = init_db_connection()
 
 ### Logic
 
-
 def entrypoint(request):
-    if request.method == "GET":
-        return get_movies(request)
+    if request.path == "/recommend" and request.method == "GET":
+        return create_recommendation()
+
     elif request.method == "POST":
         return save_movie()
-
+    
+    elif request.method == "GET":
+        print(request.path)
+        return get_movies(request)
 
 def get_movies(request):
     global db
@@ -79,6 +84,51 @@ def get_movies(request):
                 "plot": row["plot"]
             })
     return jsonify(ret)
+
+def create_recommendation():
+    recommendations = []
+    with db.connect() as conn:
+        df_uratings = pd.read_sql("SELECT * from ratings", conn)
+        df_movies = pd.read_sql("SELECT * from movies", conn)
+
+        users = set(df_uratings["user"])
+        movies = set(df_uratings["id"])
+
+        # User based
+        user_rating_matrix = calculate_user_rating_matrix(users, movies, df_uratings)
+        # Item based
+        #item_based_user_rating_matrix = calculate_user_rating_matrix(users, movies, df_uratings, False)
+
+        for user in users:
+            #User based
+            correct_wrong_user_data(user, user_rating_matrix)
+            #Item based
+            #correct_wrong_user_data(user, item_based_user_rating_matrix, False)
+
+        # User based
+        user_movie_watch_matrix, user_unwatched_movies_matrix = compare_user_ratings(user_rating_matrix, movies, users)
+        similarity_matrix = calculate_similarity_matrix(user_movie_watch_matrix, user_rating_matrix, users)
+        movie_rating_matrix = calculate_movie_rating_matrix(movies, user_rating_matrix)
+        # Item based
+        #item_based_user_movie_watch_matrix, item_based_user_unwatched_movies_matrix = compare_user_ratings(item_based_user_rating_matrix, movies, users, False)
+        #item_based_similarity_matrix = calculate_similarity_matrix(item_based_user_movie_watch_matrix, item_based_user_rating_matrix, movies)
+        #item_based_movie_rating_matrix = calculate_movie_rating_matrix(users, item_based_user_rating_matrix)
+        
+        for user in users:
+            recommendation_matrix = calculate_recommendation_matrix(user, movie_rating_matrix, similarity_matrix, user_rating_matrix, user_unwatched_movies_matrix)
+            #item_based_recommendation_matrix = calculate_recommendation_matrix(user, item_based_movie_rating_matrix, item_based_similarity_matrix, item_based_user_rating_matrix, #item_based_user_unwatched_movies_matrix, user_rating_matrix, False)
+            #print('The user-based recommendations for ' + user + ' are: ' + str(map_movie_id_to_title(getRecommendations(recommendation_matrix), df_movies)))
+            #print('The item-based recommendations for ' + user + ' are: ' + str(map_movie_id_to_title(getRecommendations(item_based_recommendation_matrix), df_movies)))
+
+            recommendations.append(
+                {
+                    "user": user,
+                    "movies": map_movie_id_to_title(getRecommendations(recommendation_matrix), df_movies)
+                }
+            )
+
+    return jsonify(recommendations)
+
     
 def save_movie():
     global db
