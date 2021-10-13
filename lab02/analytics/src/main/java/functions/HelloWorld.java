7d7d6560b1057532e6287e2870df529662a30ea4
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-10-13 22:59:13 +0200

    add activejdbc and include it

--- lab02/analytics/src/main/java/functions/HelloWorld.java
+++ lab02/analytics/src/main/java/functions/HelloWorld.java
@@ -3,33 +3,83 @@ package functions;
 import com.google.cloud.functions.HttpFunction;
 import com.google.cloud.functions.HttpRequest;
 import com.google.cloud.functions.HttpResponse;
+import org.javalite.activejdbc.Base;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class HelloWorld implements HttpFunction {
+
     // Simple function to return "Hello World"
     @Override
     public void service(HttpRequest request, HttpResponse response)
             throws IOException {
-        BufferedWriter writer = response.getWriter();
-        Template template = new Template("analytics.html");
-        template.loadTemplate();
+
+        Template template;
         Map<String, Object> context = new HashMap<>();
-        Map<String, String> analytics = Map.of(
-                "Average movie rating", "",
-                "Highest rated movie(s)", "",
-                "Lowest rated movie(s)", "",
-                "Most rated movie(s)", "",
-                "Least rated movie(s)", "",
-                "Most user ratings", "",
-                "Least user ratings", "",
-                "Total ratings", "",
-                "Total movies", ""
-        );
-        context.put("stats", analytics);
+        BufferedWriter writer = response.getWriter();
+        var requestParam = request.getFirstQueryParameter("password");
+        if (requestParam.isPresent() && requestParam.get().equals("password")) {
+            template = new Template("analytics.html");
+            template.loadTemplate();
+            queryData(context);
+
+        } else {
+            template = new Template("unauthorized.html");
+            template.loadTemplate();
+        }
         writer.write(template.renderTemplate(context));
+
+    }
+
+    private void queryData(Map<String, Object> context) {
+        String url = String.format("jdbc:mysql://%s:3306/%s",
+                System.getenv("SQL_HOST"), System.getenv("SQL_DB"));
+
+        String userName = System.getenv("SQL_USER");
+        String password = System.getenv("SQL_PASSWORD");
+
+        Base.open("com.mysql.cj.jdbc.Driver", url, userName, password);
+        if (Base.hasConnection()) {
+            List<Movie> movies = Movie.findAll();
+            setMovieContext(movies, context);
+        }
+        Base.close();
+    }
+
+    private void setMovieContext(List<Movie> movies, Map<String, Object> context) {
+        MovieComparator comparator = new MovieComparator();
+        movies.sort(comparator);
+        Map<String, String> stats = new HashMap<>();
+
+        Movie bestRating = movies.get(movies.size() - 1);
+        Movie worstRating = movies.get(0);
+
+        String bestRatingString =
+                String.format("%s: %.1f", bestRating.get("title"), bestRating.getDouble("rating"));
+        String worstMovieString =
+                String.format("%s: %.1f", worstRating.get("title"), worstRating.getDouble("rating"));
+
+
+        Double avg = movies.stream().mapToDouble(value -> value.getDouble("rating")).average().getAsDouble();
+
+        stats.put("Highest rated movie: ", bestRatingString);
+        stats.put("Lowest rated movie: ", worstMovieString);
+        stats.put("Average movie rating: ", avg.toString());
+        stats.put("Total movies: ", String.valueOf(movies.size()));
+
+
+        context.put("stats", stats);
+    }
+
+    private class MovieComparator implements Comparator<Movie> {
+        @Override
+        public int compare(Movie o1, Movie o2) {
+            return o1.getDouble("rating").compareTo(o2.getDouble("rating"));
+        }
     }
 }
---
+++ lab02/analytics/src/main/java/functions/Ratings.java
@@ -0,0 +1,6 @@
+package functions;
+
+import org.javalite.activejdbc.Model;
+
+public class Ratings extends Model {
+}
