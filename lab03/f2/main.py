273c48c762ceca5d59d37de617f353435320017f
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-10-20 22:43:53 +0200

    remove jsonify again

--- lab03/f2/main.py
+++ lab03/f2/main.py
@@ -5,7 +5,7 @@ def entrypoint(request):
     if request.path == "/":
         data = request.json
         np.random.shuffle(data)
-        return jsonify(data)
+        return data
 
     elif request.path == "/ping":
         return ('', 204)
