ae51e05fe2be14ce4a68c77d7b40c2ad00230fed
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-12-30 20:54:29 +0100

    implement timer logic

--- lab08/frontend/src/App.vue
+++ lab08/frontend/src/App.vue
@@ -2,22 +2,27 @@
   <div class="container has-text-centered">
     <div class="columns is-flex is-flex-wrap-wrap is-vcentered is-fullheight">
       <div class="column is-12">
-        <span class="title">{{interval}}:{{(seconds).toString().padStart(2, 0)}}
+        <span class="title" v-if="intervalMinutes > 0 || intervalSeconds > 0">
+          Bewegen in {{intervalMinutes}}:{{(intervalSeconds).toString().padStart(2, 0)}}
+        </span>
+
+        <span class="title" v-else>
+          Bewegen für {{durationMinutes}}:{{(durationSeconds).toString().padStart(2, 0)}} Minuten
         </span>
       </div>
       <div class="column is-12">
         <div class="level">
           <div class="level-item">
             <label for="interval">
-              <span class="mr-2">Alle {{interval}} Minuten bewegen</span>
-              <input class="input" type="number" name="interval" v-model="interval">
+              <span class="mr-2">Alle {{intervalMinutes}} Minuten bewegen</span>
+              <input class="input" type="number" name="interval" v-model="intervalMinutes">
             </label>
           </div>
 
           <div class="level-item">
             <label for="duration">
               <span class="mr-2">Wie viele Minuten wollen Sie sich bewegen?</span>
-              <input class="input" type="number" name="duration" v-model="duration">
+              <input class="input" type="number" name="duration" v-model="durationMinutes">
             </label>
           </div>
         </div>
@@ -33,43 +38,44 @@
 </template>
 
 <script setup lang="ts">
-interface TimerData {
-  interval: number;
-  duration: number;
-  timestamp: number;
-}
-
 import { ref } from "@vue/reactivity";
-const duration = ref(5)
-const interval = ref(60)
-let minutes = 0;
-let seconds = ref(0);
-let internalInterval: null | number = null;
+import { Ref } from "vue";
+const durationMinutes = ref(5);
+const durationSeconds = ref(0);
+const intervalMinutes = ref(60)
+const intervalSeconds = ref(0);
+let intervalResetValue = 0;
+let durationResetValue = 0;
 
 function startTimer(): void {
-  minutes = interval.value;
-  const timerData: TimerData = {
-    interval: minutes,
-    duration: duration.value,
-    timestamp: Date.now()
-  }
+  intervalResetValue = intervalMinutes.value;
+  durationResetValue = durationMinutes.value;
   
-  internalInterval = setInterval(startInterval, 1000);
+  setInterval(startInterval, 1000);
   
 }
 
 function startInterval(): void {
-  if (seconds.value > 0 && interval.value >= 0) {
-    seconds.value--
-  } else if (interval.value > 0) {
-    seconds.value = 59
-    interval.value--
-  } else if (internalInterval) {
-    clearInterval(internalInterval)
-    // call api
+  if (intervalMinutes.value === 0 && intervalSeconds.value === 0) {
+    if (durationMinutes.value === 0 && durationSeconds.value === 0) {
+      intervalMinutes.value = intervalResetValue;
+      durationMinutes.value = durationResetValue;
+    } else {
+      updateTimer(durationMinutes, durationSeconds);
+    }
+  } else {
+    updateTimer(intervalMinutes, intervalSeconds);
   }
-  console.log(`Minutes: ${interval.value} Seconds: ${seconds.value}`);
-  
+}
+
+function updateTimer(_minutes: Ref<number>, _seconds: Ref<number>) {
+  if (_seconds.value > 0 && _minutes.value >= 0) {
+    _seconds.value--
+  } else if (_minutes.value > 0) {
+    _seconds.value = 5
+    _minutes.value--
+  }
+  console.log(`Minutes: ${_minutes.value} Seconds: ${_seconds.value}`);
 }
 
 </script>
