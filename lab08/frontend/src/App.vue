05ebd821124a3da9fc07b48a73f058eb2c431198
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-12-30 20:15:15 +0100

    implement basic frontend

--- lab08/frontend/src/App.vue
+++ lab08/frontend/src/App.vue
@@ -1,30 +1,83 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="container has-text-centered">
+    <div class="columns is-flex is-flex-wrap-wrap is-vcentered is-fullheight">
+      <div class="column is-12">
+        <span class="title">{{interval}}:{{(seconds).toString().padStart(2, 0)}}
+        </span>
+      </div>
+      <div class="column is-12">
+        <div class="level">
+          <div class="level-item">
+            <label for="interval">
+              <span class="mr-2">Alle {{interval}} Minuten bewegen</span>
+              <input class="input" type="number" name="interval" v-model="interval">
+            </label>
+          </div>
+
+          <div class="level-item">
+            <label for="duration">
+              <span class="mr-2">Wie viele Minuten wollen Sie sich bewegen?</span>
+              <input class="input" type="number" name="duration" v-model="duration">
+            </label>
+          </div>
+        </div>
+      </div>
+
+      <div class="column is-12 is-flex is-justify-content-center">
+        <div class="buttons">
+          <button class="button is-success" @click="startTimer">Timer starten</button>
+        </div>
+      </div>
+    </div>
   </div>
-  <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script setup lang="ts">
+interface TimerData {
+  interval: number;
+  duration: number;
+  timestamp: number;
 }
 
-#nav {
-  padding: 30px;
+import { ref } from "@vue/reactivity";
+const duration = ref(5)
+const interval = ref(60)
+let minutes = 0;
+let seconds = ref(0);
+let internalInterval: null | number = null;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+function startTimer(): void {
+  minutes = interval.value;
+  const timerData: TimerData = {
+    interval: minutes,
+    duration: duration.value,
+    timestamp: Date.now()
   }
+  
+  internalInterval = setInterval(startInterval, 1000);
+  
+}
+
+function startInterval(): void {
+  if (seconds.value > 0 && interval.value >= 0) {
+    seconds.value--
+  } else if (interval.value > 0) {
+    seconds.value = 59
+    interval.value--
+  } else if (internalInterval) {
+    clearInterval(internalInterval)
+    // call api
+  }
+  console.log(`Minutes: ${interval.value} Seconds: ${seconds.value}`);
+  
+}
+
+</script>
+
+<style>
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css";
+
+.is-fullheight {
+  height: 100vh;
 }
 </style>
